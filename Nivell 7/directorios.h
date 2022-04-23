be7176f32a2ec86273cfa22244fdcf16bad615ae
Atomic_Lettuce <xvivesm@gmail.com>
2022-04-23 14:09:01 +0200

    NIVELL 7 ACABAT. falta leer_sf

--- Nivell 7/directorios.h
+++ Nivell 7/directorios.h
@@ -1,11 +1,12 @@
 #include "ficheros.h"
 #define TAMNOMBRE 60
-#define TAMENTRADA 60+4
+#define TAMENTRADA 60 + 4
 
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo);
 int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsigned int *p_inodo, unsigned int *p_entrada, char reservar, unsigned char permisos);
 void mostrar_error_buscar_entrada(int error);
-struct entrada{
+struct entrada
+{
     char nombre[TAMNOMBRE];
     unsigned int ninodo;
 };
