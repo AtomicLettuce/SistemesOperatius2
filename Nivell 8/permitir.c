fad57a260494b694c9096d61bfee6d440baa4dce
Atomic_Lettuce <xvivesm@gmail.com>
2022-05-11 11:05:45 +0200

    afegit fitxers que no hi eren

--- Nivell 8/permitir.c
+++ Nivell 8/permitir.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
         }
 
         // Desmontar dispositivo
-        if (bmount() == -1) {
+        if (bumount() == -1) {
 			
             perror("ERROR");
 			return -1;
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
     }else{
 
         // Mala Sintaxi
-        perror("ERROR");
+        printf("Sintaxis: permitir <nombre_dispositivo> <ninodo> <permisos>\n");
 	    return -1;
 
     }
